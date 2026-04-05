3d4994d8870b934c9015955453ce08d57d94f350
Joshua <joshuaprpich@gmail.com>
2026-04-05 10:05:21 -0500

    fourth

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it Third time" />
+      <HelloWorld msg="You did it Fourth time" />
     </div>
   </header>
 
